77aa177bad523b484f2120643a0fb711c2a9745f
maheshkumarsaini8769-del <maheshkumarsaini8769@gmail.com>
2026-08-26 14:53:35 +0530

    Fix website search navigation + admin sidebar CSS

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ function CustomerSite() {
   return (
     <SiteProvider>
       <Loader />
-      <Navbar />
+      <Navbar onSearchProduct={handleMaterialClick} />
       <main>
         <Hero />
         <Stats />
