61a767583865bf019f0fcb9b280ac4adbce7b1bf
maheshkumarsaini8769-del <maheshkumarsaini8769@gmail.com>
2026-08-26 15:08:55 +0530

    Add all-sales route

--- src/App.jsx
+++ src/App.jsx
@@ -130,6 +130,7 @@ function App() {
             <Route path="reviews" element={<AdminReviews />} />
             <Route path="stock" element={<AdminStock />} />
             <Route path="sales" element={<AdminSales />} />
+            <Route path="all-sales" element={<AdminSales />} />
             <Route path="cash-sales" element={<AdminSales saleTypeFilter="cash" />} />
             <Route path="online-sales" element={<AdminSales saleTypeFilter="online" />} />
             <Route path="purchases" element={<AdminPurchases />} />
